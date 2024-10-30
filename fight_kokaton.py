14acc8c7117b8969fa93dfc85dddf0c4cf88d6b3
C0A23027 <c0a2302749@edu.teu.ac.jp>
2024-10-30 11:19:00 +0900

    Stage(1-1)の実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -63,7 +63,8 @@ class Bird(pg.sprite.Sprite):
         引数1 num：こうかとん画像ファイル名の番号
         引数2 screen：画面Surface
         """
-        self.image = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        self.image = pg.transform.rotozoom(
+            pg.image.load(f"fig/{num}.png"), 0, 0.9)
         screen.blit(self.image, self.rect)
 
     def update(self, key_lst: list[bool], screen: pg.Surface):
@@ -89,7 +90,8 @@ class Beam:
     """
     こうかとんが放つビームに関するクラス
     """
-    def __init__(self, bird:"Bird"):
+
+    def __init__(self, bird: "Bird"):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん（Birdインスタンス）
@@ -107,13 +109,14 @@ class Beam:
         """
         if check_bound(self.rct) == (True, True):
             self.rct.move_ip(self.vx, self.vy)
-            screen.blit(self.img, self.rct)    
+            screen.blit(self.img, self.rct)
 
 
 class Bomb:
     """
     爆弾に関するクラス
     """
+
     def __init__(self, color: tuple[int, int, int], rad: int):
         """
         引数に基づき爆弾円Surfaceを生成する
@@ -140,6 +143,7 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+
 class Score:  # 現状問題なし
     def __init__(self):
         self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
@@ -148,9 +152,10 @@ class Score:  # 現状問題なし
         self.rct.center = [100, 50]
         self.point = 0
 
-    def update(self, screen:pg.Surface): 
-        self.img = self.fonto.render(f"スコア：{self.point}", 0,(0, 0, 255))
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render(f"スコア：{self.point}", 0, (0, 0, 255))
         screen.blit(self.img, [100, HEIGHT-50])
+
 
 class Wall(pg.sprite.Sprite):
     '''
@@ -159,30 +164,78 @@ class Wall(pg.sprite.Sprite):
     xy: 壁
     height: 壁の縦幅
     '''
+
     def __init__(self, xy: tuple[int, int], width: int, height: int):
         super().__init__()
-        self.image = pg.Surface([width,height])
-        pg.draw.rect(self.image, (255,0,0), (0, 0, width ,height))
+        self.image = pg.Surface([width, height])
+        pg.draw.rect(self.image, (255, 0, 0), (0, 0, width, height))
         self.rect = self.image.get_rect()
-        self.rect.center = xy
+        self.rect.topleft = xy
 
-    def update(self, screen:pg.Surface):
+    def update(self, screen: pg.Surface):
         screen.blit(self.image, self.rect)
+
+
+class Stage:
+    '''
+    ステージを生成するためのクラス
+    '''
+
+    def __init__(self):
+        # 壁のリストを作成
+        self.walls = pg.sprite.Group()
+        self.goals = pg.sprite.Group()
+        # 壁を生成
+        wall_settings = [
+            ((0, 0), WIDTH, 20),      # 上の壁
+            ((0, HEIGHT-20), WIDTH, 20),  # 下の壁
+            ((0, 0), 20, HEIGHT),     # 左の壁
+            ((WIDTH-20, 0), 20, HEIGHT),  # 右の壁
+
+            ((100, 100), 5, 550),
+            ((100, 100), WIDTH-400, 5),
+            ((WIDTH-300, 100), 5, HEIGHT-100),
+            ((WIDTH-220, 0), 5, HEIGHT)
+
+
+        ]
+
+        # リストを使って壁を生成
+        for setting in wall_settings:
+            self.walls.add(Wall(*setting))
+        self.goal = Wall((WIDTH-300, HEIGHT-40), 80, 10)
+        self.goal.image.fill((0, 255, 0))
+        self.goals.add(self.goal)
+
+    def draw_grid(self, screen: pg.Surface, grid_size: int):
+        """
+        グリッドを描画するメソッド
+        引数 screen: 描画対象のSurface
+        引数 grid_size: グリッドのサイズ（ピクセル）
+        """
+        for x in range(0, WIDTH, grid_size):
+            pg.draw.line(screen, (0, 0, 0), (x, 0), (x, HEIGHT))  # 垂直線
+        for y in range(0, HEIGHT, grid_size):
+            pg.draw.line(screen, (0, 0, 0), (0, y), (WIDTH, y))  # 水平線
+
+    def update(self, screen: pg.Surface):
+        for wall in self.walls:
+            if isinstance(wall, MovingWall):
+                wall.update()
+        self.walls.draw(screen)
+        pg.display.update()
 
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))    
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bird = Bird((300, 200))
-    beam = None
-    beams = []
-    # bomb = Bomb((255, 0, 0), 10)
-    bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
-    walls = pg.sprite.Group()
-    walls.add(Wall([100,100]
-                ,30,78))
-    walls.draw(screen)
+    bird = Bird((50, HEIGHT-50))
+    # beam = None
+    # beams = []
+    # # bomb = Bomb((255, 0, 0), 10)
+    # bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    stage = Stage()
     score = Score()
     clock = pg.time.Clock()
     tmr = 0
@@ -190,59 +243,66 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)   
-                beams.append(beam)     
+
+            # if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            #     # スペースキー押下でBeamクラスのインスタンス生成
+            #     beam = Beam(bird)
+            #     beams.append(beam)
         screen.blit(bg_img, [0, 0])
-        
-        for bomb in bombs:
-            if bird.rect.colliderect(bomb.rct):
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(8, screen)
-                fonto = pg.font.Font(None, 80)
-                txt = fonto.render("Game Over", True, (255, 0, 0))
-                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
-                pg.display. update()  
-                time.sleep(5)
-                return
-            
-        for j, bomb in enumerate(bombs):
-            for i, beam in enumerate(beams): 
-                if beam is not None:
-                    if beam.rct.colliderect(bomb.rct):
-                        beams[i], bombs[j] = None, None
-                        bird.change_img(6,screen)
-                        score.point += 1
-                        pg.display.update()
-            beams = [beam for beam in beams if beam  is not None]
-        bombs = [bomb for bomb in bombs if bomb is not None]
-        
-        for i, beam in enumerate(beams):
-            if check_bound(beam.rct) != (True, True):
-                beams[i] = None
-                pg.display.update() 
-        beams = [beam for beam in beams if beam  is not None]
-        if len(pg.sprite.spritecollide(bird,walls,False)) !=0:
-                        # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+
+        # for bomb in bombs:
+        #     if bird.rect.colliderect(bomb.rct):
+        #         # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+        #         bird.change_img(8, screen)
+        #         fonto = pg.font.Font(None, 80)
+        #         txt = fonto.render("Game Over", True, (255, 0, 0))
+        #         screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+        #         pg.display. update()
+        #         time.sleep(5)
+        #         return
+
+        # for j, bomb in enumerate(bombs):
+        #     for i, beam in enumerate(beams):
+        #         if beam is not None:
+        #             if beam.rct.colliderect(bomb.rct):
+        #                 beams[i], bombs[j] = None, None
+        #                 bird.change_img(6, screen)
+        #                 score.point += 1
+        #                 pg.display.update()
+        #     beams = [beam for beam in beams if beam is not None]
+        # bombs = [bomb for bomb in bombs if bomb is not None]
+
+        # for i, beam in enumerate(beams):
+        #     if check_bound(beam.rct) != (True, True):
+        #         beams[i] = None
+        #         pg.display.update()
+        # beams = [beam for beam in beams if beam is not None]
+        if len(pg.sprite.spritecollide(bird, stage.walls, False)) != 0:
             bird.change_img(8, screen)
             fonto = pg.font.Font(None, 80)
             txt = fonto.render("Game Over", True, (255, 0, 0))
             screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
-            pg.display. update()  
+            pg.display.update()
             time.sleep(5)
             return
-
-
+        if len(pg.sprite.spritecollide(bird, stage.goals, False)) != 0:
+            bird.change_img(8, screen)
+            fonto = pg.font.Font(None, 80)
+            txt = fonto.render("Game Clear", True, (255, 0, 0))
+            screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+            pg.display.update()
+            time.sleep(5)
 
         key_lst = pg.key.get_pressed()
-        bird.update(key_lst, screen)  
-        for beam in beams :
-            beam.update(screen)
-        for bomb in bombs:
-            bomb.update(screen)
+        bird.update(key_lst, screen)
+        # for beam in beams:
+        #     beam.update(screen)
+        # for bomb in bombs:
+        #     bomb.update(screen)
+        stage.draw_grid(screen, grid_size=100)
         score.update(screen)
-        walls.draw(screen)
+        stage.goals.draw(screen)
+        stage.walls.draw(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
